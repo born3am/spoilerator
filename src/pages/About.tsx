e10a809e9431b549e3e8fd360e7009ba2ffec6a8
Pedró Sìlvá <psmaranhao@gmail.com>
2025-02-19 15:07:46 +0100

    feat(About): enhance content and styling for About page
    Updated the About page with new content and improved styles.
    Added responsive design elements and utilized CSS variables for
    consistent theming. Enhanced user experience with better layout
    and visual appeal.

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,8 +4,22 @@ import './About.css';
 const About: React.FC = () => {
   return (
     <div className="about-content">
-      <h2>About</h2>
-      <p>This is a demo service built by <strong>born3am</strong> using <strong><a href="https://developer.themoviedb.org/docs/getting-started" target="_blank" rel="noopener noreferrer">The Movie DB API</a></strong> and <strong><a href="https://docs.mistral.ai/api/" target="_blank" rel="noopener noreferrer">Mistral AI API</a></strong>.</p>
+      <p>
+        Welcome to <strong>The Spoilerator</strong>! This application is designed for the impatient, the curious, and the malicious who want to get the spoilers before the movie even starts.
+      </p>
+      <p>
+        It was built in few days with the help of AI models (Mistral and GPT-4o), and it also uses Mistral AI API to generate the spoilers. The application is powered by TMDB API to fetch the movie details and posters.
+      </p>
+      <h2>Stack Used</h2>
+      <p className='about-content__stack'>
+        React, TypeScript, Vite, Cline, Github Copilot, TMDB API, Mistral AI API.
+      </p>
+      <p>
+        This project is open-source and available on <a href="https://github.com/born3am/spoilerator" target="_blank" rel="noopener noreferrer">GitHub</a>. Feel free to explore the code, contribute, or provide feedback.
+      </p>
+      <p>
+        Built with ❤️ by <strong>Born3am</strong>.
+      </p>
     </div>
   );
 };

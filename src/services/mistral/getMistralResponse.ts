bf6264e4ac54224551211f109bbbc5a934ab546e
Pedró Sìlvá <psmaranhao@gmail.com>
2025-03-03 00:09:20 +0100

    feat: add mistral api cache in localstorage

--- src/services/mistral/getMistralResponse.ts
+++ src/services/mistral/getMistralResponse.ts
@@ -7,6 +7,7 @@ interface MistralResponse {
 
 const API_CALL_LIMIT = 10; // Maximum number of API calls allowed per minute
 const RATE_LIMIT_WINDOW = 60000; // 1 minute in milliseconds
+const CACHE_EXPIRATION = 300000; // 5 minutes in milliseconds
 
 const getApiCallTimestamps = (): number[] => {
   return JSON.parse(localStorage.getItem('apiCallTimestamps') || '[]');
@@ -20,6 +21,37 @@ const cleanUpOldTimestamps = () => {
   const oneMinuteAgo = Date.now() - RATE_LIMIT_WINDOW;
   let timestamps = getApiCallTimestamps().filter((ts) => ts > oneMinuteAgo);
   setApiCallTimestamps(timestamps);
+};
+
+// Remove all expired cache
+const removeExpiredCache = () => {
+  const now = Date.now();
+
+  // Iterate through all keys in localStorage
+  Object.keys(localStorage).forEach((key) => {
+    if (key.startsWith('cache_')) {
+      const cachedData = localStorage.getItem(key);
+      if (cachedData) {
+        const { timestamp } = JSON.parse(cachedData);
+        
+        if (now - timestamp >= CACHE_EXPIRATION) {
+          localStorage.removeItem(key);
+        }
+      }
+    }
+  });
+};
+
+const getCachedResponse = (key: string): MistralResponse | null => {
+  const cachedData = localStorage.getItem(`cache_${key}`);
+  if (!cachedData) return null;
+  const { response } = JSON.parse(cachedData);
+  
+  return { response };
+};
+
+const setCachedResponse = (key: string, response: string) => {
+  localStorage.setItem(`cache_${key}`, JSON.stringify({ response, timestamp: Date.now() }));
 };
 
 export const getMistralResponse = async (
@@ -38,12 +70,17 @@ export const getMistralResponse = async (
     throw new Error('MISTRAL API Key is not defined in environment variables.');
   }
 
+  removeExpiredCache();
   cleanUpOldTimestamps();
   let apiCallTimestamps = getApiCallTimestamps();
 
   if (apiCallTimestamps.length >= API_CALL_LIMIT) {
     throw new Error('Spoilerator usage limit exceeded, try again in a minute.');
   }
+
+  const cacheKey = `${title}_${releaseDate}`;
+  const cachedResponse = getCachedResponse(cacheKey);
+  if (cachedResponse) return cachedResponse;
 
   const model = 'mistral-small-latest';
   const client = new Mistral({ apiKey });
@@ -59,6 +96,7 @@ export const getMistralResponse = async (
 
     apiCallTimestamps.push(Date.now());
     setApiCallTimestamps(apiCallTimestamps);
+    setCachedResponse(cacheKey, response.choices[0].message.content);
 
     return { response: response.choices[0].message.content };
   } catch (error) {
